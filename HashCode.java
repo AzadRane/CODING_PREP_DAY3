33a883653890b08d067a1c4a1466f85b430d83bb
Harish Rane <raneharish11@gmail.com>
2023-04-28 23:38:30 +0530

    adding hashcode file

---
+++ HashCode.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class HashCode
+{
+    public static void main(String args[])
+    {
+        int arr[] = new int[5]; //crea6ting an integer array of 5 size
+
+
+        int hashcode= arr.hashCode();
+
+        System.out.println("Arrays Hascode is :"+hashcode);
+
+        System.out.println("Arrays reference is :"+arr);
+
+    }
+}
